95a509399818a0279f65a4a3a238f58ab3bd74fa
Colliname <yugiohduel3@gmail.com>
2022-12-17 10:04:35 +0100

    progress over the last week or so

---
+++ js/index.js
@@ -0,0 +1,48 @@
+//sticky effect
+window.onscroll = function () {
+  myFunction();
+};
+
+var navbar = document.getElementById("navbar");
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+//ttt button
+function toTheTop() {
+  window.scrollTo(0, 0);
+}
+
+// slider
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("Slides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+setInterval(function () {
+  plusSlides(1);
+}, 5000);
